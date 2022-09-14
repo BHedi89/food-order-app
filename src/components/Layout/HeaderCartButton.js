cf09a556d8c637df94919dc9f8b8c423fafd378a
BHedi89 <hedi.besenyei89@gmail.com>
2022-09-14 14:56:15 +0200

    remove item from cart and finish up the app

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -4,13 +4,32 @@ import CartIcon from "../Cart/CartIcon";
 import CartContext from "../../store/cart-context";
 
 const HeaderCartButton = (props) => {
+    const [btnIsHighlighted, setbtnHighlighted] = React.useState(false);
     const cartCtx = React.useContext(CartContext);
+    const {items} = cartCtx;
     const numberOfCartItems = cartCtx.items.reduce((currNum, item) => {
         return currNum + item.amount;
     }, 0);
+    
+    const btnClasses = `${classes.button} ${btnIsHighlighted ? classes.bump: ''}`;
+
+    React.useEffect(() => {
+        if(cartCtx.items.length === 0){
+            return;
+        }
+        setbtnHighlighted(true);
+
+        const timer = setTimeout(() => {
+            setbtnHighlighted(false);
+        }, 300);
+
+        return () => {
+            clearTimeout(timer);
+        };
+    }, [items])
 
     return (
-        <button className={classes.button} onClick={props.onClick}>
+        <button className={btnClasses} onClick={props.onClick}>
             <span className={classes.icon}>
                 <CartIcon />
             </span>
